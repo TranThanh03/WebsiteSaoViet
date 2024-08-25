4a8b0e5827133129717299d385d05c3e5f274de7
thevietnguyen <thevietnguyen89@gmail.com>
2024-08-25 18:00:31 +0700

    Update index.php

--- Views/calendar/index.php
+++ Views/calendar/index.php
@@ -1,52 +1,52 @@
 <!-- calendar -->
 <div class="calendar">
     <div class="title">
-        <span>Lịch đặt</span>
+        <span>Lịch đặt tour</span>
         <button type="button" id="btn-close"><img src="./public/icons/xmark-solid.svg" alt="icon"></button>
     </div>
     <div class="content">
-        <?php if(isset($_SESSION['sdt'])):?>
-            <?php foreach($data as $calendar):?>
+        <?php if(isset($_SESSION['user_phone'])):?>
+            <?php foreach($data as $booking):?>
                 <div class="sub-content">
                     <div class="icon-sub">
                         <img src="./public/icons/calendar-regular.svg" alt="icon">
                     </div>
                     <div class="infor-calendar">
                         <div>
-                            <p>Mã phiếu khám: <span id="id-calendar"><?php echo $calendar['MaLichKham']?></span></p>
+                            <p>Mã tour: <span id="booking-id"><?php echo $booking['TourBookingID']?></span></p>
                         </div>
                         <div>
-                            <p>Thời gian khám: <span id="time">
+                            <p>Ngày đặt: <span id="booking-date">
                                 <?php 
-                                    $date = date('d/m/Y', strtotime($calendar['NgayKham']));
-                                    echo $calendar['ThoiGianKham'] . " | ". $date;
+                                    $date = date('d/m/Y', strtotime($booking['BookingDate']));
+                                    echo $date;
                                 ?>
                             </span></p>
                         </div>
                         <div>
-                            <p>Phòng khám: <span id="room"><?php echo $calendar['TenPhongKham']?></span></p>
+                            <p>Ngày khởi hành: <span id="departure-date"><?php echo date('d/m/Y', strtotime($booking['DepartureDate']))?></span></p>
                         </div>
                         <div>
-                            <p>Khoa: <span id="name-faculty"><?php echo $calendar['TenKhoa']?></span></p>
+                            <p>Điểm đến: <span id="destination"><?php echo $booking['Destination']?></span></p>
                         </div>
                         <div>
-                            <p>Bác sĩ: <span id="name-doctor"><?php echo $calendar['TenBS']?></span></p>
+                            <p>Hướng dẫn viên: <span id="guide-name"><?php echo $booking['GuideName']?></span></p>
                         </div>
                         <div>
-                            <p>Trạng thái: <span id="status"><?php echo $calendar['TenTrangThai']?></span></p>
+                            <p>Trạng thái: <span id="status"><?php echo $booking['BookingStatus']?></span></p>
                         </div>
                     </div>
                     <div class="cancel">
-                        <p><a href="index.php?controller=calendar&action=delete&id=<?php echo $calendar['MaLichKham']?>"><span>Hủy lịch</span></a></p>
+                        <p><a href="index.php?controller=tour&action=delete&id=<?php echo $booking['TourBookingID']?>"><span>Hủy tour</span></a></p>
                     </div>
                 </div>
             <?php endforeach;?>
         <?php endif;?>
-        <?php if(!isset($_SESSION['sdt'])):?>
-            <h3>Chưa có thông tin</h3>
+        <?php if(!isset($_SESSION['user_phone'])):?>
+            <h3>Chưa có thông tin đặt tour</h3>
         <?php endif;?>
     </div>
     <div class="total">
-        <p>Tổng: <span id="totalCalendar"></span></p>
+        <p>Tổng: <span id="totalBookings"></span></p>
     </div>
 </div>
